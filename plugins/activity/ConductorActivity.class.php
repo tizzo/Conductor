75f8eca2e3c6adde34685090178ad58a6c922630
Howard Tyson <howard@zivtech.com>
2011-07-22 16:29:57 -0400

    updated the ctools_plugin hooks to get our plugins actually working, relocated the plugin info for the include

--- plugins/activity/ConductorActivity.class.php
+++ plugins/activity/ConductorActivity.class.php
@@ -1,12 +1,7 @@
 <?php
 
-$plugin = array(
-  'title' => t('Administrative repository listing'),
-  'description' => t(''),
-  'handler' => array(
-    'class' => 'ConductorActivity',
-  ),  
-);
-
+/**
+ *
+ */
 class ConductorActivity {
 }
