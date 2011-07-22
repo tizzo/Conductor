29d8f9c0e2454911611bfa629711354da148fb32
Howard Tyson <howard@zivtech.com>
2011-07-22 14:04:43 -0400

    added plugins directory and a stub for the activity class

---
+++ plugins/activity/ConductorActivity.class.php
@@ -0,0 +1,12 @@
+<?php
+
+$plugin = array(
+  'title' => t('Administrative repository listing'),
+  'description' => t(''),
+  'handler' => array(
+    'class' => 'ConductorActivity',
+  ),  
+);
+
+class ConductorActivity {
+}
