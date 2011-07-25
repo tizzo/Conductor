680619dc9a2703ccc5db71baa10832797973394a
Howard Tyson <howard@howardtyson.com>
2011-07-25 19:54:35 -0400

    added methods to ConductorActivity

--- plugins/activity/activity/ConductorActivity.class.php
+++ plugins/activity/activity/ConductorActivity.class.php
@@ -4,4 +4,49 @@
  *
  */
 class ConductorActivity {
+
+  //
+  public $inputs = array();
+
+  //
+  public $outputs = array();
+
+  //
+  public $x = null;
+
+  // 
+  public $y = null;
+
+  public function addInput($activity) {
+  }
+
+  public function addOutput($activity) {
+  }
+
+  /**
+   * TODO:
+   */
+  public function configureForm() {
+    return FALSE;
+  }
+
+  /**
+   * TODO:
+   */
+  public function configureFormValidate() {
+  }
+
+  /**
+   * TODO:
+   */
+  public function configureFormSubmit() {
+  }
+
+  public function run() {
+  }
+
+  public function getId() {
+    // TODO: Consider whether this could be made any smarter...
+    return drupal_hash_base64($this) . REQUEST_TIME;
+  }
 }
