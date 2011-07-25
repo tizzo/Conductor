ae0d2af2354341887bc155a65b73887e2c46d447
Howard Tyson <howard@howardtyson.com>
2011-07-25 19:50:44 -0400

    added tpl to render activities, updated js to make the draggable, retitled page callback

--- conductor_ui/js/conductor_ui.js
+++ conductor_ui/js/conductor_ui.js
@@ -2,7 +2,7 @@
 
 Drupal.behaviors.conductor_ui = {
   attach: function (context, settings) {
-    
+    $('.conductor-ui-activity', context).draggable(); 
   }
 }
 
---
+++ conductor_ui/theme/conductor-activity.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ */
+?>
+<div class="conductor-ui-activity">
+  <span><?php print check_plain($name); ?></span>
+</div>
