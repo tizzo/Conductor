d584154b090d71ab5cd820dd43d5215a2d96b639
Howard Tyson <howard@howardtyson.com>
2011-07-26 23:51:08 -0400

    added a class variable to the activity template

--- conductor_ui/theme/conductor-activity.tpl.php
+++ conductor_ui/theme/conductor-activity.tpl.php
@@ -3,6 +3,6 @@
  * @file
  */
 ?>
-<div class="conductor-ui-activity">
+<div class="conductor-ui-activity <?php print check_plain($class); ?>">
   <span><?php print check_plain($name); ?></span>
 </div>
