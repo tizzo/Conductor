99e17ad715dd3dfe1a4f7055b92dd71c571893da
Howard Tyson <howard@howardtyson.com>
2011-08-08 09:50:42 -0400

    add ctools_drop_buttons for activity configuration, css styling and stole button styling from views

--- conductor_ui/theme/conductor-activity.tpl.php
+++ conductor_ui/theme/conductor-activity.tpl.php
@@ -5,4 +5,5 @@
 ?>
 <div class="conductor-ui-activity <?php print check_plain($class); ?>">
   <span><?php print check_plain($name); ?></span>
+  <div class="conductor-drop-link"><?php print $drop_links; ?></div>
 </div>
--- plugins/activity/activity/ConductorActivity.class.php
+++ plugins/activity/activity/ConductorActivity.class.php
@@ -44,4 +44,16 @@ class ConductorActivity {
 
   public function run() {
   }
+
+  /**
+   *
+   */
+  public function getUILinks() {
+    // Create an array to be rendered by theme_links__ctools_dropbutton().
+    $links = array();
+    $links['edit'] = array('title' => t('edit'), 'href' => $_GET['q']);
+    $links['input'] = array('title' => t('add input'), 'href' => $_GET['q']);
+    $links['output'] = array('title' => t('add output'), 'href' => $_GET['q']);
+    return $links;
+  }
 }
