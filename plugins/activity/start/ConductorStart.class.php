353f17d3d6649512a0f388b6e565fbb40e964f60
Howard Tyson <howard@howardtyson.com>
2011-09-02 18:57:25 -0400

    added start and stop activities

---
+++ plugins/activity/start/ConductorStart.class.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ *
+ */
+class ConductorStart extends ConductorActivity {
+
+  //
+  public $inputs = array();
+
+  //
+  public $outputs = array();
+
+  //
+  public $x = null;
+
+  // 
+  public $y = null;
+
+  public function option_definition() {
+    $options = parent::option_definition();
+    unset($options['inputs']);
+    return $options;
+  }
+
+  public function addInput($activity) {
+    return FALSE;
+  }
+
+  public function addOutput($activity) {
+  }
+
+  /**
+   * TODO:
+   */
+  public function configureForm() {
+    return FALSE;
+  }
+
+  /**
+   * TODO:
+   */
+  public function configureFormValidate() {
+  }
+
+  /**
+   * TODO:
+   */
+  public function configureFormSubmit() {
+  }
+
+  public function run() {
+  }
+
+  /**
+   *
+   */
+  public function getUILinks() {
+    // Create an array to be rendered by theme_links__ctools_dropbutton().
+    $links = parent::getUILinks();
+    unset($links['input']);
+    return $links;
+  }
+
+}
---
+++ plugins/activity/stop/ConductorStop.class.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ *
+ */
+class ConductorStop extends ConductorActivity {
+
+  //
+  public $inputs = array();
+
+  //
+  public $outputs = array();
+
+  //
+  public $x = null;
+
+  // 
+  public $y = null;
+
+  public function option_definition() {
+    $options = parent::option_definition();
+    unset($options['outputs']);
+    return $options;
+  }
+
+  public function addInput($activity) {
+  }
+
+  public function addOutput($activity) {
+    return FALSE;
+  }
+
+  public function run() {
+  }
+
+  /**
+   *
+   */
+  public function getUILinks() {
+    // Create an array to be rendered by theme_links__ctools_dropbutton().
+    $links = parent::getUILinks();
+    unset($links['output']);
+    return $links;
+  }
+
+}
