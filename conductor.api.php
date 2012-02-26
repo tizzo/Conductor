e8bccc9e4032d111ccc8fa5f1c16b9e0e84bba15
Howard Tyson <howard@howardtyson.com>
2012-02-26 11:02:32 -0500

    Moved the code to explicitly set each handler in a workflow into the api script because we are simplifying the tests.

--- conductor.api.php
+++ conductor.api.php
@@ -8,7 +8,7 @@
 /**
  * Implements hook_conductor_activity_process().
  *
- * Allows modules to modify the 
+ * Allows modules to modify the
  *
  * @param $plugin
  *   The ctools plugin data.
@@ -18,3 +18,45 @@ function hook_conductor_activity_process(&$plugin) {
     'foo' => 'bar',
   );
 }
+
+/**
+ * Usually workflow component objects are setup automatically, here is an example of
+ * how to explicitly set the handlers in a workflow.
+ */
+function example_manual_workflow_factory() {
+  $workflow = $this->getWorkflow($name);
+  // Instantiate the workflow instance for processing.
+  $instance = new ConductorInstance();
+  // Instantiate our mock storage handler.
+  $storage = new conductorStorageTest;
+  // Provide our mock storage handler with an array to use as a persistent
+  // data store.
+  $storage->setDatabase($this->dataStore);
+  // Set the storage handler for this instance.
+  $instance->setStorage($storage);
+  // Actually assign the workflow instance state to the workflow.
+  $workflow->setState($instance);
+  // Create the context
+  $context = array('foo' => 'bar');
+  // Get the first activity, we will need it to set the initial context.
+  $start = $workflow->getActivity('start');
+  // Instantiate the workflow instance for processing.
+  $instance = new ConductorInstance();
+  // Instantiate our mock storage handler.
+  $storage->setDatabase($this->dataStore);
+  // Set the storage handler for this instance.
+  $instance->setStorage($storage);
+  // Create a state object for the first activity.
+  $startState = new ConductorActivityState($start, $instance);
+  // Set the context.
+  $startState->context = $context;
+  // Add the activity's state to the workflow instance state.
+  $instance->setActivityStates(array('start' => $startState));
+  // Actually assign the workflow instance state to the workflow.
+  $workflow->setState($instance);
+  // Set up the observer for tracking workflow history in tests.
+  $observer = new ConductorObserverTest;
+  $observer->history = &$history;
+  $workflow->state->registerObserver($observer);
+
+  }
