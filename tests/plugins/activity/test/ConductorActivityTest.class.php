a45300fcfd8dc66a205aa05b43e20121c7c56659
Howard Tyson <howard@howardtyson.com>
2012-02-25 22:34:37 -0500

    Beginning to generalize the test plugin.

--- tests/plugins/activity/test/ConductorActivityTest.class.php
+++ tests/plugins/activity/test/ConductorActivityTest.class.php
@@ -3,24 +3,18 @@
 /**
  * This is a test activity to perform different actions depending on the context data.
  */
-class ConductorActivityPauseTest extends ConductorActivity {
+class ConductorActivityTest extends ConductorActivity {
 
   /**
    * Implements ConductorActivity::run().
    */
   public function run() {
-
-    //if ($this->state->getContext('pauseTest') === TRUE) {
+    if ($this->state->getContext($this->name . ':pause') == TRUE && $this->state->getContext($this->name . ':resume') == FALSE) {
       $this->activityState->markSuspended();
-    //}
-  }
-
-  /**
-   * The start method performs no actions.
-   */
-  public function process() {
-    // TODO: Start returning other the pause status.
-    return TRUE;
+    }
+    else {
+      $this->activityState->markCompeted();
+    }
   }
 
 }
